609fa76dee66fc26bf5ad57c78ec86ebe50ac7b1
Amrsamy19 <amrsamy622@gmail.com>
2020-11-23 23:00:02 +0200

    Adding the basic structure

---
+++ src/NotificationTemplate.java
@@ -0,0 +1,21 @@
+public class NotificationTemplate {
+    private String name;
+    private String type;
+    private String content;
+
+    public void createTemplate(){
+
+    }
+
+    public void readTemplate(){
+
+    }
+
+    public void updateTemplate(){
+
+    }
+
+    public void deleteTemplate(){
+
+    }
+}
